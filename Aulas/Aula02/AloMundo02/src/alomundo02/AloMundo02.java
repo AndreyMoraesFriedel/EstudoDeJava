50593c06e051ef9e73c6537a96ae10df8357abcf
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-07 15:16:16 -0300

    Aula02 - Caracteres De Formatação

---
+++ Aulas/Aula02/AloMundo02/src/alomundo02/AloMundo02.java
@@ -0,0 +1,14 @@
+package alomundo02;
+
+public class AloMundo02 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int nota1 = 7, nota2 = 9, nota3 = 6;
+            float media = (nota1 + nota2 + nota3) / 3;
+            System.out.printf("%s%n","HelloWorld");
+            System.out.printf("Ola %s, um feliz %d, parabens pelo seu %.2f%n", "Andrey", 2024, 10.0);
+            System.out.printf("Media do Aluno:%.2f, devido as notas:%d, %d e %d", media, nota1, nota2, nota3);
+    }
+    
+}
