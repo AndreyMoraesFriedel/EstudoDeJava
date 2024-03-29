4e58b2d2b0543866c5b7ca0a465981b11a2badef
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-03-29 18:40:53 -0300

    Aula19 - Classes Abstratas

---
+++ Aulas/Aula19/ClassesAbstratas/src/conhecimento/CursoJava.java
@@ -0,0 +1,12 @@
+package conhecimento;
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+        
+        Animal an1 = new Animal();
+        an1.info();
+
+    }
+
+}
---
+++ Aulas/Aula19/ClassesAbstratas/src/conhecimento/Formiga.java
@@ -0,0 +1,5 @@
+package conhecimento;
+
+public class Formiga {
+
+}
